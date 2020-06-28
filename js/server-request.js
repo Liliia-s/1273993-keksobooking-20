624d5b06d908d3fdd628af6d221525eacb39fb84
Liliia <befolife@gmail.com>
2020-06-28 22:36:12 +0300

    Дополнила XHR доп статусами HTTP

--- js/server-request.js
+++ js/server-request.js
@@ -3,7 +3,11 @@
 (function () {
   var xhrNew;
   var StatusCode = {
-    OK: 200
+    OK: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500
   };
 
   var requestData = function (successHandler, errorHandler, method, URL) {
@@ -12,11 +16,35 @@
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
-        successHandler(xhr.response);
-      } else {
-        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      var error;
+      switch (xhr.status) {
+        case StatusCode.OK:
+          successHandler(xhr.response);
+          break;
+
+        case StatusCode.BAD_REQUEST:
+          error = 'Плохой запрос';
+          break;
+        case StatusCode.UNAUTHORIZED:
+          error = 'Пользователь не авторизован';
+          break;
+        case StatusCode.NOT_FOUND:
+          error = 'Запрашиваемый ресурс не найден';
+          break;
+        case StatusCode.SERVER_ERROR:
+          error = 'Внутренняя ошибка сервера';
+          break;
+        default:
+          error = 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText;
       }
+      if (error) {
+        errorHandler(error);
+      }
+      // if (xhr.status === StatusCode.OK) {
+      //   successHandler(xhr.response);
+      // } else {
+      //   errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      // }
     });
 
     xhr.addEventListener('error', function () {
